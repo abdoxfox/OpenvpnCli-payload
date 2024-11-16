e5c2e58f7e533c1ef32f9fa97fb712840c6d8dba
abdoxfox <46646744+abdoxfox@users.noreply.github.com>
2024-11-16 02:38:29 +0100

    Update OpenVPNRun.bat

--- OpenVPNRun.bat
+++ OpenVPNRun.bat
@@ -6,8 +6,9 @@ set "destPath=%PROGRAMFILES%\OpenVPN\config"
 if not exist "%sourcefile%" echo no config found in this path & exit
 copy "%sourcefile%" "%destPath%"
 echo OpenVPN config copied successfully 
-
+start /b python server.py
+timeout /t 2 /nobreak > nul
 for %%F in ("%sourcefile%") do set cfgName=%%~nxF
-"%PROGRAMFILES%\OpenVPN\bin\openvpn-gui.exe" --connect cfgName
+"%PROGRAMFILES%\OpenVPN\bin\openvpn-gui.exe" --connect "%cfgName%"
 del "%destPath%\client.ovpn"
 pause
